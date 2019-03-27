0a173736eee3e903ae1b02b58a396296ad1a901d
Roberto Metere <roberto@metere.it>
2019-03-27 15:28:33 +0000

    declassify tactic added
    (that's the easy one)

--- src/phl/ecPhlLeakable.ml
+++ src/phl/ecPhlLeakable.ml
@@ -10,10 +10,13 @@
 (* -------------------------------------------------------------------- *)
 open EcUtils
 open EcFol
+open EcPath
 open EcTypes
 open EcEnv
 open EcDecl
 open EcModules
+
+open EcBigInt
 
 open EcCoreGoal
 open EcLowGoal
@@ -23,11 +26,48 @@ module Sid = EcIdent.Sid
 module CI = EcCoreLib
 
 (* -------------------------------------------------------------------- *)
-let path_to_expr path env =
+let rec expr_to_string e = match e.e_node with (* [[remove me]] *)
+  | Eint   (i) -> "Eint(" ^ (EcBigInt.to_string i) ^ ")"
+  | Elocal (_) -> "Elocal"
+  | Evar   (v) -> "Evar(" ^ (EcTypes.string_of_pvar v) ^ ")"
+  | Eop    (p, tl) -> "Eop(" ^ (EcPath.tostring p) ^ ", [" ^ (List.fold_left (^) "" (List.map ((^) ":") (List.map dump_ty tl))) ^ "])"
+  | Eapp   (e, el) -> "Eapp(" ^ (expr_to_string e) ^ ", [" ^ (List.fold_left (^) "" (List.map ((^) ", ") (List.map expr_to_string el))) ^ "])"
+  | Equant (_) -> "Equant"
+  | Elet   (_) -> "Elet"
+  | Etuple (l) -> "Etuple(" ^ (List.fold_left (^) "," (List.map expr_to_string l)) ^ ")"
+  | Eif    (c, t, e) -> "Eif(" ^ (expr_to_string c) ^ ", then " ^ (expr_to_string t) ^ ", else " ^ (expr_to_string e) ^ ")"
+  | Ematch (e, el, t) -> "Ematch(" ^ (expr_to_string e) ^ ", [" ^ (List.fold_left (^) " " (List.map ((^) ", ") (List.map expr_to_string el))) ^ "], type: " ^ (dump_ty t) ^ ")"
+  | Eproj  (t, i) -> "Eproj(" ^ (expr_to_string t) ^ " at [" ^ (string_of_int i) ^ "])"
+
+let lvalue_to_string lv = match lv with (* [[remove me]] *)
+  | LvVar   (v, t) -> "Variable " ^ (string_of_pvar v)
+  | LvTuple (l) -> "[tuple" ^ (List.fold_left (^) ", " (List.map string_of_pvar (List.map fst l))) ^ "]"
+  | LvMap   ((p, tl), v, e, t) -> "Map " ^ (string_of_pvar v) ^ " {" ^ (expr_to_string e) ^ "}"
+  
+(* -------------------------------------------------------------------- *)
+let path_to_expr path env = (* [[remove me]] *)
   let module E = struct exception Abort end in
   let opkind = (Op.by_path path env).op_kind in
   match opkind with
   | OB_oper(Some (OP_Plain(e))) -> e
+  | _ -> raise E.Abort
+
+(* -------------------------------------------------------------------- *)
+let destr_ty_map e : ty * ty = (* [[move me to ecTypes?]] *)
+  let module E = struct exception Abort end in
+  match e.e_node with
+  | Eapp (e, _) -> (match e.e_node with
+    | Eop (op, tl) when List.length tl = 2
+        -> (List.nth tl 0, List.nth tl 1)
+    | _ -> raise E.Abort)
+  | _ -> raise E.Abort
+
+(* -------------------------------------------------------------------- *)
+let destr_map e : expr * prog_var = (* [[move me to ecTypes?]] *)
+  let module E = struct exception Abort end in
+  match e.e_node with
+  | Eapp (e, vl) when List.length vl = 2
+      -> (List.nth vl 1, destr_var (List.nth vl 0))
   | _ -> raise E.Abort
 
 (* -------------------------------------------------------------------- *)
@@ -48,23 +88,21 @@ let t_equiv_declassify_sided_r side tc =
     | `Left  -> es.es_ml, es.es_sl
     | `Right -> es.es_mr, es.es_sr
   in
-  let (lv, rv), s' = tc1_last_secasgn tc s in
-  let ty = proj_leakable_ty env (rv.e_ty) in
-  let e_inst_op = path_to_expr CI.CI_Leakable.p_inst env in (* of type leakable['a] -> 'a *)
-  let e_inst = e_app e_inst_op [rv] ty in (* of type Top.Y[] *)
-  let e_leaked = path_to_expr CI.CI_Leakable.p_min_leakage env in
-(*   Printf.printf "%s\n" (dump_ty e_inst.e_ty); *)
-  let assignment = s_asgn (lv, e_tuple [e_inst; e_leaked]) in
-  let s' = s_seq s' assignment in
-  let rv = EcFol.form_of_expr (EcMemory.memory m) rv in
-  let declassification = f_is_leaked ty rv in
-  let post = es.es_po in
-(*   let assignment = lv_subst m lv in *)
-(*   let assignment = mk_let_of_lv_substs ~uselet:false env ([], post) in *)
-(*   let assignment = f_eq assignment assignment in *)
-(*   let post = f_andas [declassification; post] in *)
   
-  Printf.printf "%d\n" (tc1_pos_last_secasgn tc s);
+  let (lv, leakable), s' = tc1_last_secasgn tc s in
+  let ty_leakable = proj_leakable_ty env (e_ty leakable) in
+  let e_inst = e_proj leakable 0 ty_leakable in
+  let e_distr = e_proj leakable 1 (tdistr ty_leakable) in
+  let e_leaked = e_op CI.CI_Leakable.p_leaked [] tconfidentiality in
+  let map_lv = List.nth (snd (split_args leakable)) 0 in
+  let xty, mty = destr_ty_map map_lv in
+  let ex, vm = destr_map map_lv in
+  let mlv = LvMap((CI.CI_Map.p_set, [xty; mty]), vm, ex, mty) in
+  let declassification = s_asgn (mlv, e_tuple [e_inst; e_distr; e_leaked]) in
+  let assignment = s_asgn (lv, e_inst) in
+  let s' = s_seq s' declassification in
+  let s' = s_seq s' assignment in
+  let post = es.es_po in
   
   let concl =
     match side with
